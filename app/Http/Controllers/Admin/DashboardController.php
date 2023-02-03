322eef69160e5d1873f63f5e7ecf54fa23da0038
ThuzarMyint10 <thuzarmyint795@gmail.com>
2023-02-03 11:40:43 +0630

    barchat

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers\Admin;
 
+use DateTime;
+use DateTimeImmutable;
 use App\Models\Product;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Models\Attendent;
+use App\Models\Trainer;
 use App\Models\Customer;
+use App\Models\Attendent;
+use Illuminate\Http\Request;
 use App\Models\PaymentRecord;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $data['attendencedMembers'] = Attendent::where('status', '1')->get();
+        $expiredDate = '';
+
         $data['members'] = Customer::get();
         $prices = 0;
         foreach (PaymentRecord::get() as $paymentPrice) {
@@ -21,15 +26,71 @@ class DashboardController extends Controller
         }
         $data['price'] = $prices;
         $data['paymentRecords'] = PaymentRecord::get();
+        $paymentRecords = PaymentRecord::get();
+
+        // For Attendenced Members
+        $todayDate = new DateTimeImmutable(Carbon::now()->format('Y-m-d'));
+        $data['attendencedMembers'] = Attendent::where(
+            'attendent_date',
+            $todayDate
+        )->get();
+
+        // For Bar Chart
+        $monthlyEarnings = PaymentRecord::select('price', 'record_date')
+            ->get()
+            ->groupBy(function ($date) {
+                //return Carbon::parse($date->created_at)->format('Y'); // grouping by years
+                return Carbon::parse($date->record_date); // grouping by months
+            });
+        $monthlyPrice = 0;
+        foreach ($monthlyEarnings as $key => $monthlyEarning) {
+            foreach ($monthlyEarning as $earning) {
+                $monthlyPrice += (int) $earning->price;
+            }
+            $month[] = Carbon::parse($key)->format('F');
+            $monthlyEarningMoney[
+                Carbon::parse($key)->format('F')
+            ] = $monthlyPrice;
+            $monthlyPrice = 0;
+        }
+        $data['monthlyEarningMoney'] = $monthlyEarningMoney;
+        $data['month'] = $month;
+        // End of Bar Chart
+
+        // For Expired Payment Member
+        foreach ($paymentRecords as $paymentRecord) {
+            $packageName = $paymentRecord->package->package;
+            $packageDate = (int) explode('month', $packageName)[0];
+            $packageExpiredDate = new DateTimeImmutable(
+                Carbon::parse($paymentRecord->record_date)->addMonths(
+                    $packageDate
+                )
+            );
+
+            // $paymentDate = new DateTimeImmutable($paymentRecord->record_date);
+            // @dd($packageExpiredDate);
+            // @dd($todayDate);
+            $interval = date_diff($todayDate, $packageExpiredDate);
+            if ($interval->invert) {
+                $expiredDate = -$interval->days;
+            } else {
+                $expiredDate = $interval->days;
+            }
+
+            if ($expiredDate <= 3) {
+                $data['expiredPaymentMember'] = Customer::where(
+                    'id',
+                    $paymentRecord->customer_id
+                )->get();
+            }
+        }
+        $data['trainers'] = Trainer::get();
 
         return view('admin.dashboard.index', $data);
     }
-    public function create()
+    public function show(int $memberId)
     {
-        return view('admin.dashboard.create');
-    }
-
-    public function barchart(Request $request)
-    {
+        $memberDetails = Customer::where('id', $memberId)->get();
+        return view('members.profile', compact('memberDetails'));
     }
 }
--- resources/views/admin/dashboard/memberlist.blade.php
+++ resources/views/admin/dashboard/memberlist.blade.php
@@ -19,39 +19,44 @@
               <div class="tab-content " id="pills-tabContent">
                 <div class="tab-pane fade show active" id="pills-active" role="tabpanel" aria-labelledby="pills-active-tab" tabindex="0">
                   <div class="container bg-grey p-2 my-2 border col-md-12">
-                   @forelse ($attendencedMembers as $attendencedMember)
-                      @if($attendencedMember->customer)
+                   @forelse ($attendencedMembers
+            ->take(3) as $attendencedMember)
+                      @if($attendencedMember->member)
                         <div class="row col-md-12">
                           <div class="item-thumbnail col-md-5">
-                            <img src="{{asset('/uploads/customer/'.$attendencedMember->customer->image)}}" alt="image" class="img-fluid">
+                            <img src="{{asset('/uploads/customer/'.$attendencedMember->member->image)}}" alt="image" class="img-fluid">
                           </div>
                           <div class="col-md-6 my-3">
-                            <h5>{{ $attendencedMember->customer->name }}</h5>
-                            <p>Height : {{$attendencedMember->customer->height }}cm</p>
-                            <p>Weight : {{$attendencedMember->customer->weight }}</p>
+                            <h5>{{ $attendencedMember->member->name }}</h5>
+                            <p>Height : {{$attendencedMember->member->height }}cm</p>
+                            <p>Weight : {{$attendencedMember->member->weight }}</p>
                           </div>
                         </div> 
                        @else
                           <div>No Attended Member</div>
                        @endif 
                    @empty
+                        
                        <div>No Attended Member</div>
                    @endforelse
                   </div>
                 </div>
                 <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="pills-register-tab" tabindex="0">
                   <div class="container bg-grey p-2 my-2 border col-md-12">
-                   @forelse ($members as $member)
-                        <div class="row col-md-12">
-                      <div class="item-thumbnail col-md-5">
-                        <img src="{{asset('/uploads/customer/'.$member->image)}}" alt="image" class="img-fluid">
-                      </div>
-                      <div class="col-md-6 my-3">
-                      <h5>{{ $member->name }}</h5>
-                      <p>Height : {{ $member->height }}cm</p>
-                      <p>Weight : {{ $member->weight }}</p>
-                      </div>
-                    </div>   
+                   @forelse ($members
+            ->take(3) as $member)
+                      <a href="{{ url('members/'.$member->id) }}" target="_blank" style="text-decoration: none;">
+                        <div class="row col-md-12 text-dark">
+                          <div class="item-thumbnail col-md-5">
+                            <img src="{{asset('/uploads/customer/'.$member->image)}}" alt="image" class="img-fluid">
+                          </div>
+                          <div class="col-md-6 my-3">
+                          <h5>{{ $member->name }}</h5>
+                          <p>Height : {{ $member->height }}cm</p>
+                          <p>Weight : {{ $member->weight }}</p>
+                          </div>
+                        </div> 
+                      </a>  
                    @empty
                        <div>No Registered Member</div>
                    @endforelse

e2083b5d0eefc0788ac7a301847b871e76abdb44
ThuzarMyint10 <thuzarmyint795@gmail.com>
2023-02-10 11:49:03 +0630

    modified barchart

--- resources/views/admin/customers/viewinvoice.blade.php
+++ resources/views/admin/customers/viewinvoice.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Payment Receipt</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    {{--  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">  --}}
     </head>
 </head>
 <body>
